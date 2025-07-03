f611667cc79fed4b02ddc4490eb3c69431713416
shiva1656 <shiva955064@gmail.com>
2025-07-04 00:12:21 +0530

    "Added a change in the service of Hotel."

--- src/main/java/com/projects/airBnbApp/service/HotelService.java
+++ src/main/java/com/projects/airBnbApp/service/HotelService.java
@@ -5,6 +5,7 @@ import com.projects.airBnbApp.entity.Hotel;
 
 public interface HotelService {
 
+    //Checking weather github is working or not
     HotelDto createNewHotel(HotelDto hotelDto);
     HotelDto getHotelById(Long id);
 }
