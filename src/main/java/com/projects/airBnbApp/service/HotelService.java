55438a0cf0e88595a4bbba2ee927135f668aa716
shiva1656 <shiva955064@gmail.com>
2025-07-04 10:44:15 +0530

    "Checking weather GitHub its working or not and made changes in the HotelService "

--- src/main/java/com/projects/airBnbApp/service/HotelService.java
+++ src/main/java/com/projects/airBnbApp/service/HotelService.java
@@ -6,6 +6,7 @@ import com.projects.airBnbApp.entity.Hotel;
 public interface HotelService {
     //Just checking weather git hub is working or not.
     //Just trying to push.
+    //Try making changes
     HotelDto createNewHotel(HotelDto hotelDto);
     HotelDto getHotelById(Long id);
 }
